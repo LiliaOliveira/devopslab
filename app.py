754b3b74abef6fba29c382f8847776f2a4256331
LiliaOliveira <114241508+LiliaOliveira@users.noreply.github.com>
2022-10-09 23:27:56 -0300

    Update Mensagem Entrega

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO v02"
+    return "Entrega Solution Sprint - RM 342202"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
